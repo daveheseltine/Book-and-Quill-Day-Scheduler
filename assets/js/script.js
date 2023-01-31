76cbcbb3c4c9741b6b4833f9fa00b796739c2812
Dave Heseltine <daveomcdave@gmail.com>
2023-01-31 15:09:53 +0000

    Added clock with moment().

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
+// # Define Global Variables.
+// You can edit the start/end times presented on the webpage!:
+var start = 9;
+var end = 18;
+
+
 // jQuery
 $(document).ready(function () {
 
 
-  // jQuery can now be used here.
+// # Initialise the webpage
+  // ## Set the clock to the current time:
+  $("#currentDay").text(moment().format("hh:mm A, dddd Do MMMM"));
 
-
+  // ## After 1s, and every second thereafter, update the current time and detect if the correct colours are displayed (which will take effect if the current hour changes):
+  $(setInterval(function () {
+    $("#currentDay").text(moment().format("hh:mm A, dddd Do MMMM"));
+  }
+  , 1000));
 });
