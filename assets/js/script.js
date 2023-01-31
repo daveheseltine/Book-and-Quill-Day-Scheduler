c2a422aff503da63b8716a18a38172cb0c37da4a
Dave Heseltine <daveomcdave@gmail.com>
2023-01-31 15:13:23 +0000

    Added JavaScript to generate HTML for Time-Block elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,14 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("hh:mm A, dddd Do MMMM"));
   }
   , 1000));
+
+  // ## Generate HTML for the Time-Blocks:
+  for (var i = start; i < end; i++) {
+    $(".container").append($("<div class='row time-block' id='hour" + [i] + "'></div>"));
+    $("#hour" + [i])
+      .append($("<div class='col-md-1 col-2 hour'>" + moment().startOf("day").add([i], "hours").format("h A") + "</div>"))
+      .append($("<textarea class='col-md-10 col-8 description id='text" + [i] + "'></textarea>"))
+      .append($("<button class='col-md-1 col-2 btn saveBtn'><i class='fas fa-save'></i></button>"))
+    ;
+  }
 });
